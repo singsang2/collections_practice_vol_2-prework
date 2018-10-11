86a45e8fb08f5d0b7c5f0558187ecb9013e2a938
Sung Bae <singsang2@gmail.com>
2018-10-11 13:21:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
 # your code goes here
+def begins_with_r(array)
+  array.each do |element|
+    if !element.start_with?("r")
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  list = []
+  array.each do |element|
+    list << element if element.include?("a")
+  end
+  list
+end
+
+def first_wa(array)
+  array.each do |element|
+    if element.slice(0,2) == "wa"
+      return element
+    end
+  end
+  nil
+end
+
+def remove_non_strings(array)
+  index = array.size - 1
+  until index < 0
+    array.delete_at(index) if array[index].class != String
+    index -= 1
+  end
+  array
+end
+
+def count_elements(array)
+
+end
+
+def merge_data(keys, data)
+  index = 0
+  hash = {}
+  while index < keys.size
+    hash[keys[index].to_sym] = data[index]
+    index += 1
+  end
+  hash
+end

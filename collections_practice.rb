1ee3533d240edbbeee1c5cafc80037664d162a6d
Sung Bae <singsang2@gmail.com>
2018-10-11 15:48:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,7 +54,7 @@ def merge_data(keys, data)
   index = 0
   merged = []
   while index < keys.length
-    keys.each {|element| puts data[index].values}
+    keys.each {|element| puts element.merge(data[index].values.first)}
     puts index
     index += 1
   end
